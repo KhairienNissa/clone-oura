c7f37ca2ae9e40ff429263ee5e4ac7f31f551865
KhairienNissa <khairien.nissa@cbncloud.co.id>
2022-08-29 11:09:15 +0700

    up

--- components/atom/DropDownNav.js
+++ components/atom/DropDownNav.js
@@ -9,42 +9,41 @@ function DropDownNav() {
   const [aktif, setAktif] = useState(false);
   return (
     <>
-     
-        <div className="text-sm flex" onClick={() => setAktif(!aktif)}>
+      <div className="text-sm flex" onClick={() => setAktif(!aktif)}>
         <div className="text-md mr-1 mt-0.5">
-                      <BiCalculator />
-                    </div>
-         <p> Calculator ML</p>
-         <div className="-mt-0.6 text-lg">
-                      <MdOutlineArrowDropDown />
-                    </div>
+          <BiCalculator />
         </div>
-        <div
-          className={` -md:ml-4 px-3 py-3 mt-3 relative md:absolute z-10  md:w-auto border w-full rounded-sm bg-white ${
-            !aktif && "hidden"
-          } py-2`}>
-          <div className="py-3 ">
-            <div>
-              <Link href="/HpMagic">
-                <a className="text-black flex text-sm">
-                  <AiFillIdcard />
-                  <p className="-mt-1 px-2 text-sm">HP Magic Wheel</p>
-                </a>
-              </Link>
-            </div>
-          </div>
-          <div className="pb-2">
-            <div>
-              <Link href="/HpZodiac">
-                <a  className="text-black flex">
-                  <AiFillIdcard />
-                  <p className="-mt-1 px-2 text-sm">HP Zodiac</p>
-                </a>
-              </Link>
-            </div>
+        <p> Calculator ML</p>
+        <div className="-mt-0.6 text-lg">
+          <MdOutlineArrowDropDown />
+        </div>
+      </div>
+      <div
+        className={` -md:ml-4 px-3 py-3 mt-3 relative md:absolute z-10  md:w-auto border w-full rounded-sm bg-white ${
+          aktif === false && "hidden"
+        } py-2`}
+      >
+        <div className="py-3 ">
+          <div>
+            <Link href="/HpMagic">
+              <a className="text-black flex text-sm">
+                <AiFillIdcard />
+                <p className="-mt-1 px-2 text-sm">HP Magic Wheel</p>
+              </a>
+            </Link>
           </div>
         </div>
-     
+        <div className="pb-2">
+          <div>
+            <Link href="/HpZodiac">
+              <a className="text-black flex">
+                <AiFillIdcard />
+                <p className="-mt-1 px-2 text-sm">HP Zodiac</p>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
